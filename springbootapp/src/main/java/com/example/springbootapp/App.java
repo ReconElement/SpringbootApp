b9b2361cfde9c106733ef98c505e9d6773c368bb
reconelement <omkarpanda895@gmail.com>
2026-03-17 04:31:51 +0530

    Added dependencies and worked on some stuff
    Added dependencies and worked on some basic request response and parsing stuff

---
+++ springbootapp/src/main/java/com/example/springbootapp/App.java
@@ -0,0 +1,44 @@
+package com.example.springbootapp;
+import org.springframework.web.bind.annotation.*;
+import org.json.JSONObject;
+
+
+class ResponseTransfer{
+    private String message;
+    public String getMessage(){
+        return this.message;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+}
+
+@RestController
+public class App {
+    @GetMapping("/health")
+    public String health(){
+        return "Health is perfectly fine";
+    }
+
+    @RequestMapping(value = "/data" , method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseTransfer data(){
+        var response = new ResponseTransfer();
+        response.setMessage("health is perfectly fine");
+        return response;
+    }
+
+    @RequestMapping(value="/data", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseTransfer postData(@RequestBody ResponseTransfer req){
+        if(req instanceof ResponseTransfer){
+            System.out.println(true);
+        }
+        System.out.println(req);
+        ResponseTransfer data = new ResponseTransfer();
+        data.setMessage("Thanks for the request data, got it " +req.getMessage());
+        return data;
+    }
+
+}
+
---
+++ springbootapp/src/main/java/com/example/springbootapp/database/User.java
@@ -0,0 +1,5 @@
+package com.example.springbootapp.database;
+
+public class User {
+
+}
